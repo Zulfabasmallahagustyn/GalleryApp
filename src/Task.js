2a5988731a1035b099f337ee41374f03d0d11d60
Zulfabasmallahagustyn <zulfaba15@gmail.com>
2023-06-20 15:51:49 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://zulfa123.netlify.app" // TODO: replace this
+const studentName = "Zulfa Basmallah Agustyn" // TODO: replace this
+const studentId = "FE5594086" // TODO: replace this
+
+export { url, studentName, studentId }
